8fba19b85274e7ca0a06b30d6669b2c0bce3b976
RalfiBahar <ralfibahar2005@hotmail.com>
2024-06-17 15:25:43 -0700

    feat(backend): finalize Getting recordings

--- RadyoKlasikServer/app.py
+++ RadyoKlasikServer/app.py
@@ -27,4 +27,4 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='192.168.68.105', debug=True)
--- RadyoKlasikServer/routes/recording.py
+++ RadyoKlasikServer/routes/recording.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify, redirect, url_for, send_from_directory
+import mimetypes
+from flask import Blueprint, jsonify, redirect, url_for, send_from_directory, request
 from flask_login import login_required
 import time
 import os
@@ -9,6 +10,11 @@ from io import BytesIO
 import datetime
 import urllib
 import http
+from mutagen.mp3 import MP3
+from mutagen.id3 import ID3, TIT2, TPE1, TALB, APIC, error
+from werkzeug.utils import secure_filename
+import hashlib
+
 
 recording_bp = Blueprint('recording', __name__)
 
@@ -16,7 +22,6 @@ is_recording = False
 record_thread = None
 audio_data = BytesIO()
 recordings_dir = "recordings"  # Directory where recordings are stored
-output_file_path = os.path.join(recordings_dir, "recorded_stream")
 
 def record_stream(url):
     global is_recording, audio_data
@@ -69,12 +74,63 @@ def stop_recording():
             os.makedirs(recordings_dir)
         
         print('EXPORTING AUDIO SEGMENT')
-        audio_segment.export(output_file_path + '_' + str(datetime.datetime.now()).replace(' ', '') + '.mp3', format="mp3")
+        output_file_path = os.path.join(recordings_dir, str(datetime.datetime.now().strftime("%d.%m.%Y")))
+        #delete hash in name
+        file_name_w_path = output_file_path + '_LiveProgramme' +  str(hash(datetime.datetime.now()))[:6]+ '.mp3'
+        audio_segment.export(file_name_w_path, format="mp3")
         print(f"STREAM RECORDED AND SAVED AS: {output_file_path}")
+        
+        selected_artwork = request.form.get('existing-artworks')
+        curr_date = datetime.datetime.today()
+        curr_date = curr_date.strftime('%d')+ '.' + curr_date.strftime('%m') + '.' + curr_date.strftime('%Y')
+        if selected_artwork:
+            print('selected art', selected_artwork)
+            artwork_path = os.path.join(thumbnails_dir, selected_artwork)
+            
+            (artwork_path)
+            add_metadata(file_name_w_path, 'Morning Delight', f'Bant Yayini ({curr_date})',  '', artwork_path)
+        else:
+            add_metadata(file_name_w_path, 'Morning Delight', f'Bant Yayini ({curr_date})',  '', 'default_artwork.jpg')
+
+
+        print('metadata applied')
         start_time = None
         audio_segment = BytesIO()
 
     return redirect(url_for('dashboard.dashboard'))
+
+def add_metadata(file_path, title, artist, album, artwork_path):
+    audio = MP3(file_path, ID3=ID3)
+    
+    try:
+        audio.add_tags()
+    except error:
+        pass
+
+    audio.tags.add(
+        TIT2(encoding=3, text=title)
+    )
+    audio.tags.add(
+        TPE1(encoding=3, text=artist)
+    )
+    audio.tags.add(
+        TALB(encoding=3, text=album)
+    )
+
+    print(audio.tags.get('TPE1'))
+    
+    with open(artwork_path, 'rb') as albumart:
+        audio.tags.add(
+            APIC(
+                encoding=3,
+                mime='image/jpeg',
+                type=3, 
+                desc=u'Cover',
+                data=albumart.read()
+            )
+        )
+
+    audio.save()
 
 @recording_bp.route('/status', methods=['GET'])
 def status():
@@ -96,10 +152,60 @@ def get_recording(filename):
 
 @recording_bp.route('/recordings', methods=['GET'])
 def get_recordings_list():
+    def get_file_hash(file_content):
+        hasher = hashlib.md5()
+        hasher.update(file_content)
+        return hasher.hexdigest()
+
+    def get_metadata(filename):
+        audio = MP3(filename, ID3=ID3)
+        metadata = {
+            'title': audio.tags.get('TIT2', 'Unknown Title').text[0] if 'TIT2' in audio.tags else 'Unknown Title',
+            'album': audio.tags.get('TALB', 'Unknown Album').text[0] if 'TALB' in audio.tags else 'Unknown Album',
+            'artist': audio.tags.get('TPE1', 'Unknown Artist').text[0] if 'TPE1' in audio.tags else 'Unknown Artist',
+            'artwork': None,
+            'duration': int(audio.info.length),
+        }
+
+        for tag in audio.tags.values():
+            if isinstance(tag, APIC):
+                artwork_data = tag.data
+                artwork_hash = get_file_hash(artwork_data)
+                mime_type = tag.mime
+                extension = mimetypes.guess_extension(mime_type)
+                if extension is None:
+                    extension = ".jpg"  
+            
+                artwork_filename = f"{artwork_hash}{extension}"
+                artwork_path = os.path.join(thumbnails_dir, artwork_filename)
+                
+                if not os.path.exists(artwork_path):
+                    with open(artwork_path, 'wb') as img:
+                        img.write(artwork_data)
+                
+                metadata['artwork'] = f"/static/assets/thumbnails/{artwork_filename}"
+                break
+
+        return metadata
+
     try:
         recordings = os.listdir(recordings_dir)
-        recordings_list = [file for file in recordings if os.path.isfile(os.path.join(recordings_dir, file))]
-        return jsonify(recordings_list)
+        recordings_list = []
+        for file in recordings:
+            file_path = os.path.join(recordings_dir, file)
+            if os.path.isfile(file_path):
+                file_metadata = get_metadata(file_path)
+                recordings_list.append({
+                    'id': get_file_hash(file_metadata['title'].encode('utf-8') + file_metadata['artist'].encode('utf-8')),
+                    'filename': file,
+                    'title': file_metadata['title'],
+                    'artist': file_metadata['artist'],
+                    'album': file_metadata['album'],
+                    'artwork': file_metadata['artwork'],
+                    'duration': file_metadata['duration'],
+                    'route_to_stream_it': url_for('recording.get_recording', filename=file)
+                })
+        return jsonify({'recordings': recordings_list})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
@@ -134,5 +240,42 @@ def get_redirect_url():
         return jsonify({'url': final_url})
     else:
         return jsonify({'error': 'Failed to retrieve the redirect MP3 stream URL'}), 500
+    
+thumbnails_dir = "static/assets/thumbnails"
+if not os.path.exists(thumbnails_dir):
+    os.makedirs(thumbnails_dir)
+
+@recording_bp.route('/upload_artwork', methods=['POST'])
+def upload_artwork():
+    if 'artwork' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+    file = request.files['artwork']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    if file:
+        file_content = file.read()
+        file_hash = hashlib.md5(file_content).hexdigest()
+        
+        mime_type = file.mimetype
+        extension = mimetypes.guess_extension(mime_type)
+        if extension is None:
+            extension = ".jpg" 
+        
+        filename = f"{file_hash}{extension}"
+        
+        file_path = os.path.join(thumbnails_dir, filename)
+        with open(file_path, 'wb') as f:
+            f.write(file_content)
+        
+        return redirect(url_for('dashboard.dashboard'))
+
+@recording_bp.route('/get_artworks', methods=['GET'])
+def get_artworks():
+    try:
+        artworks = os.listdir(thumbnails_dir)
+        artworks_list = [file for file in artworks if os.path.isfile(os.path.join(thumbnails_dir, file))]
+        return jsonify(artworks_list)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
     
