a960317c082cbcaec10d04c6aceb06c36a084954
RalfiBahar <ralfibahar2005@hotmail.com>
2024-08-10 04:06:32 -0700

    refactor(frontend): move playback logic from AudioButton component to playbackContext

--- RadyoKlasikMobile/components/AudioButton.tsx
+++ RadyoKlasikMobile/components/AudioButton.tsx
@@ -1,24 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import {
   StyleSheet,
   TouchableOpacity,
   ActivityIndicator,
-  Text,
   View,
   useWindowDimensions,
 } from "react-native";
 import { Feather } from "@expo/vector-icons";
-import TrackPlayer, {
-  usePlaybackState,
-  State,
-  Capability,
-  useProgress,
-  Event,
-} from "react-native-track-player";
-import { SongData } from "../types";
-import { getRedirectedUrl } from "../helpers/getRedirectedUrl";
-import { EXPO_PUBLIC_API_URL } from "@env";
 import { usePlayback } from "../context/PlaybackContext";
+import { SongData } from "../types";
 import ProgressBar from "./ProgressBar";
 import theme from "../styles/theme";
 
@@ -29,134 +19,18 @@ interface AudioButtonProps {
 }
 
 const AudioButton = ({ audioUrl, songData, isRecording }: AudioButtonProps) => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const playbackState = usePlaybackState();
-  const { width, height } = useWindowDimensions();
-  const {
-    isPlaying,
-    setIsPlaying,
-    isFinished,
-    setIsFinished,
-    isPlayerSetup,
-    setIsPlayerSetup,
-  } = usePlayback();
+  const { width } = useWindowDimensions();
+  const { isPlaying, playAudio, isPlayerSetup, isLoading } = usePlayback();
 
-  const setupPlayer = async () => {
-    try {
-      await TrackPlayer.setupPlayer();
-      await TrackPlayer.updateOptions({
-        capabilities: [Capability.Play, Capability.Pause, Capability.Stop],
-        notificationCapabilities: [
-          Capability.Play,
-          Capability.Pause,
-          Capability.Stop,
-        ],
-      });
-      console.log("Setting Up");
-      setIsPlayerSetup(true);
-    } catch (error) {
-      console.log(error);
+  const handlePress = () => {
+    if (isPlayerSetup) {
+      playAudio(audioUrl, songData, isRecording);
     }
   };
-
-  const playAudio = async () => {
-    setIsLoading(true);
-    try {
-      const activeTrack = await TrackPlayer.getActiveTrack();
-      const activeTrackId = activeTrack?.id;
-      const trackId = songData.id;
-
-      //console.log(progress.position / progress.duration);
-      //console.log(activeTrack?.url, "cu");
-
-      let redirectedUrl = `${EXPO_PUBLIC_API_URL}/${audioUrl}`;
-      console.log("REEEED", redirectedUrl);
-      if (!isRecording) {
-        redirectedUrl = await getRedirectedUrl(audioUrl);
-      }
-      //console.log(isFinished);
-      if (activeTrackId === trackId) {
-        // Same track is already playing
-        if (playbackState.state === State.Playing) {
-          if (isRecording) {
-            await TrackPlayer.pause();
-          } else {
-            await TrackPlayer.stop();
-          }
-          setIsPlaying(false);
-        } else {
-          if (isFinished) {
-            await TrackPlayer.reset();
-            await TrackPlayer.add({
-              id: trackId,
-              url: redirectedUrl,
-              title: songData.title,
-              artist: songData.artist,
-              duration: songData.duration,
-              artwork: songData.thumb,
-              isLiveStream: !isRecording,
-            });
-          }
-          await TrackPlayer.play();
-          setIsPlaying(true);
-        }
-      } else {
-        // New track, reset and play
-        await TrackPlayer.reset();
-        await TrackPlayer.add({
-          id: trackId,
-          url: redirectedUrl,
-          title: songData.title,
-          artist: songData.artist,
-          duration: songData.duration,
-          artwork: songData.thumb,
-          isLiveStream: !isRecording,
-        });
-        await TrackPlayer.play();
-        setIsPlaying(true);
-      }
-    } catch (error) {
-      console.error("Error playing audio:", error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  const handlePlaybackEnd = () => {
-    setIsPlaying(false);
-
-    TrackPlayer.addEventListener(Event.PlaybackQueueEnded, () => {
-      setIsPlaying(false);
-      setIsFinished(true);
-    });
-
-    TrackPlayer.addEventListener(
-      Event.PlaybackTrackChanged,
-      async ({ nextTrack }) => {
-        if (nextTrack === null) {
-          setIsPlaying(false);
-          setIsFinished(true);
-        }
-      }
-    );
-  };
-
-  useEffect(() => {
-    if (!isPlayerSetup) {
-      setupPlayer();
-    }
-    const listener = TrackPlayer.addEventListener(
-      Event.PlaybackQueueEnded,
-      handlePlaybackEnd
-    );
-    return () => {
-      listener.remove();
-    };
-  }, []);
 
   return (
     <View style={[styles.container, { width: width * 0.8 }]}>
-      <TouchableOpacity style={styles.button} onPress={playAudio}>
+      <TouchableOpacity style={styles.button} onPress={handlePress}>
         {isLoading ? (
           <ActivityIndicator size="large" color="#6b7280" />
         ) : (
--- RadyoKlasikMobile/context/PlaybackContext.tsx
+++ RadyoKlasikMobile/context/PlaybackContext.tsx
@@ -1,5 +1,19 @@
-import React, { createContext, useState, useContext, ReactNode } from "react";
-import TrackPlayer from "react-native-track-player";
+import React, {
+  createContext,
+  useState,
+  useContext,
+  ReactNode,
+  useEffect,
+} from "react";
+import TrackPlayer, {
+  Capability,
+  Event,
+  State,
+  usePlaybackState,
+} from "react-native-track-player";
+import { SongData } from "../types";
+import { getRedirectedUrl } from "../helpers/getRedirectedUrl";
+import { EXPO_PUBLIC_API_URL } from "@env";
 
 interface PlaybackContextProps {
   isPlaying: boolean;
@@ -7,8 +21,15 @@ interface PlaybackContextProps {
   isPlayerSetup: boolean;
   setIsPlayerSetup: (isPlaying: boolean) => void;
   isFinished: boolean;
-  setIsFinished: (isPlaying: boolean) => void;
+  setIsFinished: (isFinished: boolean) => void;
   resetTrack: () => Promise<void>;
+  setupPlayer: () => Promise<void>;
+  playAudio: (
+    audioUrl: string,
+    songData: SongData,
+    isRecording: boolean
+  ) => Promise<void>;
+  isLoading: boolean;
 }
 
 const PlaybackContext = createContext<PlaybackContextProps | undefined>(
@@ -31,6 +52,26 @@ export const PlaybackProvider = ({ children }: PlaybackProviderProps) => {
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
   const [isFinished, setIsFinished] = useState<boolean>(false);
   const [isPlayerSetup, setIsPlayerSetup] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const playbackState = usePlaybackState();
+
+  const setupPlayer = async () => {
+    try {
+      await TrackPlayer.setupPlayer();
+      await TrackPlayer.updateOptions({
+        capabilities: [Capability.Play, Capability.Pause, Capability.Stop],
+        notificationCapabilities: [
+          Capability.Play,
+          Capability.Pause,
+          Capability.Stop,
+        ],
+      });
+      console.log("Setting Up");
+      setIsPlayerSetup(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const resetTrack = async () => {
     try {
@@ -42,6 +83,102 @@ export const PlaybackProvider = ({ children }: PlaybackProviderProps) => {
     }
   };
 
+  const playAudio = async (
+    audioUrl: string,
+    songData: SongData,
+    isRecording: boolean
+  ) => {
+    setIsLoading(true);
+    try {
+      const activeTrack = await TrackPlayer.getActiveTrack();
+      const activeTrackId = activeTrack?.id;
+      const trackId = songData.id;
+
+      let redirectedUrl = `${EXPO_PUBLIC_API_URL}/${audioUrl}`;
+      console.log("REEEED", redirectedUrl);
+      if (!isRecording) {
+        redirectedUrl = await getRedirectedUrl(audioUrl);
+      }
+
+      if (activeTrackId === trackId) {
+        // Same track is already playing
+        if (playbackState.state === State.Playing) {
+          if (isRecording) {
+            await TrackPlayer.pause();
+          } else {
+            await TrackPlayer.stop();
+          }
+          setIsPlaying(false);
+        } else {
+          if (isFinished) {
+            await TrackPlayer.reset();
+            await TrackPlayer.add({
+              id: trackId,
+              url: redirectedUrl,
+              title: songData.title,
+              artist: songData.artist,
+              duration: songData.duration,
+              artwork: songData.thumb,
+              isLiveStream: !isRecording,
+            });
+          }
+          await TrackPlayer.play();
+          setIsPlaying(true);
+        }
+      } else {
+        // New track, reset and play
+        await TrackPlayer.reset();
+        await TrackPlayer.add({
+          id: trackId,
+          url: redirectedUrl,
+          title: songData.title,
+          artist: songData.artist,
+          duration: songData.duration,
+          artwork: songData.thumb,
+          isLiveStream: !isRecording,
+        });
+        await TrackPlayer.play();
+        setIsPlaying(true);
+      }
+    } catch (error) {
+      console.error("Error playing audio:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handlePlaybackEnd = () => {
+    setIsPlaying(false);
+
+    TrackPlayer.addEventListener(Event.PlaybackQueueEnded, () => {
+      setIsPlaying(false);
+      setIsFinished(true);
+    });
+
+    TrackPlayer.addEventListener(
+      Event.PlaybackTrackChanged,
+      async ({ nextTrack }) => {
+        if (nextTrack === null) {
+          setIsPlaying(false);
+          setIsFinished(true);
+        }
+      }
+    );
+  };
+
+  useEffect(() => {
+    if (!isPlayerSetup) {
+      setupPlayer();
+    }
+    const listener = TrackPlayer.addEventListener(
+      Event.PlaybackQueueEnded,
+      handlePlaybackEnd
+    );
+    return () => {
+      listener.remove();
+    };
+  }, []);
+
   return (
     <PlaybackContext.Provider
       value={{
@@ -52,6 +189,9 @@ export const PlaybackProvider = ({ children }: PlaybackProviderProps) => {
         isPlayerSetup,
         setIsPlayerSetup,
         resetTrack,
+        setupPlayer,
+        playAudio,
+        isLoading,
       }}
     >
       {children}
